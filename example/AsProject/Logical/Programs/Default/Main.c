3a2bdfac3e436c18792bdfd7804ae40a64b7b5b2
sclaiborne <scott@loupe.team>
2024-02-07 14:59:49 -0800

    Add manual test code to default task
    This is just for testing for now and may be removed in the future

--- example/AsProject/Logical/Programs/Default/Main.c
+++ example/AsProject/Logical/Programs/Default/Main.c
@@ -5,6 +5,8 @@
 	#include <AsDefault.h>
 #endif
 
+#include "string.h"
+
 void _INIT ProgramInit(void)
 {
 
@@ -12,7 +14,16 @@ void _INIT ProgramInit(void)
 
 void _CYCLIC ProgramCyclic(void)
 {
-
+	if(calldtoa) {
+		calldtoa = 0;
+		prve = &rve;
+		pString = stringdtoa(val, mode, ndigits, &decpt, &sign, &prve);
+		if(pString) {
+			len = strlen(pString);
+			strcpy(string, pString);
+		}
+			
+	}
 }
 
 void _EXIT ProgramExit(void)
