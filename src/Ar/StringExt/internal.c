148d9eea01fec7c4bbe7567a5517cbe3de70d136
sclaiborne <scott@loupe.team>
2024-02-08 10:44:09 -0800

    Add compiler options for semaphore methods
    I am not sure what will work best on machine yet

--- src/Ar/StringExt/internal.c
+++ src/Ar/StringExt/internal.c
@@ -5,22 +5,43 @@ extern "C"
 {
 #endif
 
-	#include "includes/rtkSemaphore.h"
+	
 
 #ifdef __cplusplus
 };
 #endif
 
+
+
+//#define USE_SEMAPHORE 
+
+#ifdef USE_SEMAPHORE
+#include "includes/rtkSemaphore.h"
+
 static __thread RTK_SEM_H criticalSection[2] = {0};
+
+#define CREATE(name, max, section) RtkCreateSemaphore(name, max, section)
+#define ACQUIRE(section, wait) RtkAcquireSemaphore(section, wait)
+#define RELEASE(section) RtkReleaseSemaphore(section)
+
+#else
+#include "includes/rtkCriticalSection.h"
+
+static __thread RTK_CRIT_SEC_H criticalSection[2] = {0};
+
+#define CREATE(name, max, section) RtkCreateCriticalSection(name, section)
+#define ACQUIRE(section, wait) RtkEnterCriticalSection(section)
+#define RELEASE(section) RtkLeaveCriticalSection(section)
+#endif
 
 void lockSelect(n) {
 	if(n > (sizeof(criticalSection)/sizeof(criticalSection[0]) - 1)) return;
-	if(!criticalSection[n]) RtkCreateSemaphore(0, 1, &criticalSection[n]);
-	RtkAcquireSemaphore(&criticalSection[n], 500L);
+	if(!criticalSection[n]) CREATE(0, 1, &criticalSection[n]);
+	ACQUIRE(&criticalSection[n], 500L);
 }
 
 void freeSelect(n) {
 	if(n > (sizeof(criticalSection)/sizeof(criticalSection[0]) - 1)) return;
 	if(!criticalSection[n]) return;
-	RtkReleaseSemaphore(&criticalSection[n]);
+	RELEASE(&criticalSection[n]);
 }
